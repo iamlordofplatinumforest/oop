8c121d3685d205bae3cd1fef102db8cbc5cf114a
Yefrosinya <efrosinasologub@gmail.com>
2025-02-16 10:51:50 +0300

    final

--- src/main/java/com/example/myversion/main.java
+++ src/main/java/com/example/myversion/main.java
@@ -10,14 +10,14 @@ public class main extends Application {
     @Override
     public void start(Stage primaryStage) {
         Pane root = new Pane();
-        lineShape line = new lineShape(10, 10, 100, 100);
+        shape line = new lineShape(10, 10, 100, 100);
         line.setBorderColor(Color.BLACK);
 
-        rectangleShape rectangle = new rectangleShape(150, 10, 150, 200);
+        shape rectangle = new rectangleShape(150, 10, 150, 200);
         rectangle.setBorderColor(Color.BLACK);
         rectangle.setInColor(Color.YELLOW);
 
-        ellipseShape ellipse = new ellipseShape(500, 110, 100, 60);
+        shape ellipse = new ellipseShape(500, 110, 100, 60);
         ellipse.setInColor(Color.LIMEGREEN);
         ellipse.setBorderColor(Color.BLACK);
 
@@ -29,7 +29,7 @@ public class main extends Application {
                 140.0, 350.0,
                 140.0, 280.0
         };
-        polygonShape polygon = new polygonShape(polygonPoints);
+        shape polygon = new polygonShape(polygonPoints);
         polygon.setInColor(Color.AQUAMARINE);
         polygon.setBorderColor(Color.BLACK);
 
@@ -39,7 +39,7 @@ public class main extends Application {
                 500.0, 250.0,
                 600.0, 300.0
         };
-        polylineShape polyline = new polylineShape(polylinePoints);
+        shape polyline = new polylineShape(polylinePoints);
         polyline.setBorderColor(Color.RED);
 
         root.getChildren().addAll(line.createShape(), rectangle.createShape(), ellipse.createShape(), polygon.createShape(), polyline.createShape());
